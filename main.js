b5d56e5ece454555f1c7b371f9c625bbe08580b9
sakunika <muthunihansani052@gmail.com>
2024-11-06 11:23:05 +0530

    landing page completed

--- main.js
+++ main.js
@@ -47,3 +47,32 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+//scroll animations
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400,
+});
+
+// sr.reveal('.header', { origin: 'top', distance: '20px', duration: 900, delay: 200 });
+sr.reveal('.nav-logo', { delay: 300 });
+
+sr.reveal('.hero-container', { origin: 'bottom', distance: '50px' });
+sr.reveal('.hero-image', { origin: 'left', delay: 500, distance: '200px' });
+sr.reveal('.hero-content', { origin: 'right', delay: 700, distance: '200px' });
+sr.reveal('.hero-content h1', { delay: 900 });
+sr.reveal('.section-button', { delay: 1000 });
+
+sr.reveal('.feature-container', { interval: 400 });
+sr.reveal('.features-image', { origin: 'left', delay: 400 });
+sr.reveal('.feature-description', { origin: 'right', delay: 400 });
+
+sr.reveal('.wrapper h1', { origin: 'bottom', distance: '30px', delay: 400 });
+sr.reveal('.faq', { interval: 200, origin: 'bottom' });
+
+sr.reveal('.footer-container', { origin: 'top', delay: 300 });
+sr.reveal('.footer-widget', { interval: 200 });
+sr.reveal('.footer-logo', { delay: 300 });
+sr.reveal('.copyright-wrapper', { delay: 200, origin: 'bottom' });
